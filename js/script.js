e35bb996aa1d1bbcc05ed880aa0a738342c1072d
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-10-18 23:08:10 +0700

    feat: add jquery

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const button = document.getElementById('button');
-const textElement = document.getElementById('text');
-
-button.addEventListener('click', function() {
-  if (textElement.innerHTML === "World") {
-    textElement.innerHTML = "Hello";
-  } else {
-    textElement.innerHTML = "World";
-  }
+$(document).ready(function() {
+    $.getJSON('https://www.themealdb.com/api/json/v1/1/categories.php', function(data) {
+      if (data.categories && data.categories.length > 0) {
+        var category = data.categories[0];
+        $('#text').append(`
+          <h1>${category.strCategory}</h1>
+        `);
+      }
+  });
 });
