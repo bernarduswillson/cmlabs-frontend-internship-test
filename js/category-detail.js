00a50e03ceda54013ae32c3f339af9f36ccbbb36
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-10-19 12:58:58 +0700

    feat: add category-detail

--- js/category-detail.js
+++ js/category-detail.js
@@ -1,6 +1,48 @@
 $(document).ready(function () {
-  var urlParams = new URLSearchParams(window.location.search);
-  var categoryDetail = urlParams.get('category');
+  // Lottie Loading Animation
+  var animation = lottie.loadAnimation({
+    container: document.getElementById('lottieAnimation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../assets/animations/spinner-loading.json'
+  });
 
-  console.log(categoryDetail);
+  var urlParams = new URLSearchParams(window.location.search);
+  var categoryName = urlParams.get('category-name');
+
+  $('#categoryName').text(categoryName);
+  $('#categoryTitle').text(categoryName + " Meals");
+
+  $.ajax({
+    url: "https://www.themealdb.com/api/json/v1/1/filter.php",
+    method: "GET",
+    data: {
+      c: categoryName
+    },
+    success: function (response) {
+      console.log(response);
+      $.each(response.meals, function (index, meal) {
+        var mealCard = $('<div></div>').load('components/meal-card.html', function () {
+          mealCard.find('#mealImage').attr('src', meal.strMealThumb);
+          mealCard.find('#mealName').text(meal.strMeal);
+
+          mealCard.click(function () {
+            var mealId = meal.idMeal;
+            window.location.href = "/meals-detail.html?meal-id=" + mealId;
+          });
+
+          $('#mealList').append(mealCard);
+        });
+      });
+    },
+
+    error: function () {
+      $('#mealTitle').text("Failed to load meals.");
+    },
+
+    complete: function () {
+      $('#lottieLoader').fadeOut();
+    }
+  });
 });
--- js/category.js
+++ js/category.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-  var categoryListTitle = $("#categoryList");
-
   // Lottie Loading Animation
   var animation = lottie.loadAnimation({
     container: document.getElementById('lottieAnimation'),
@@ -9,19 +7,21 @@ $(document).ready(function () {
     autoplay: true,
     path: '../assets/animations/spinner-loading.json'
   });
+  
+  var categoryListTitle = $("#categoryList");
 
   $.ajax({
     url: "https://www.themealdb.com/api/json/v1/1/categories.php",
     method: "GET",
     success: function (response) {
       $.each(response.categories, function (index, category) {
-        var categoryCard = $('<div></div>').load('components/card.html', function () {
+        var categoryCard = $('<div></div>').load('components/category-card.html', function () {
           categoryCard.find('#categoryImage').attr('src', category.strCategoryThumb);
           categoryCard.find('#categoryName').text(category.strCategory);
 
           categoryCard.click(function () {
             var categoryName = category.strCategory;
-            window.location.href = "/category-detail.html?category=" + categoryName;
+            window.location.href = "/category-detail.html?category-name=" + categoryName;
           });
 
           $('#categoryList').append(categoryCard);
